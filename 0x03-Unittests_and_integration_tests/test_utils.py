fc94ac9d296e1c00c56f9711a2691f13829405a0
ELIUZ <osongoeliuz14@gmail.com>
2025-07-17 21:35:18 +0300

    fixed error: empty lines

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -60,8 +60,13 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+        with patch.object(
+            TestClass,
+            'a_method',
+            return_value=42
+        ) as mock_method:
             instance = TestClass()
             self.assertEqual(instance.a_property, 42)
             self.assertEqual(instance.a_property, 42)
             mock_method.assert_called_once()
+
