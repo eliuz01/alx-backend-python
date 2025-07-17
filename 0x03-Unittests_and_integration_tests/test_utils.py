e226c87685d07d1b96022513d764743d7e066734
ELIUZ <osongoeliuz14@gmail.com>
2025-07-17 16:46:31 +0300

    fixed error: assertionerror

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python3
-import unittest
 from parameterized import parameterized
-
 from utils import access_nested_map
-
+import unittest
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Unit tests for access_nested_map function"""
-
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ("a",)),
@@ -23,4 +12,4 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), repr(path[len(context.exception.args[0]) - 1]))
+        self.assertEqual(context.exception.args[0], path[-1])
