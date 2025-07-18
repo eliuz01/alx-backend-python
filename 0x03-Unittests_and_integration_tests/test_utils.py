be79814a33e384f898803b1bf371c4e54c690c5c
ELIUZ <osongoeliuz14@gmail.com>
2025-07-18 07:58:27 +0300

    fixed error: blank lines

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -48,8 +48,6 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
 
-
-
 class TestMemoize(unittest.TestCase):
     """Tests for the memoize decorator"""
 
