e48a3f1f4c1d48e2b5419d18b5c8217fb17d5dba
ELIUZ <osongoeliuz14@gmail.com>
2025-07-20 19:53:17 +0300

    fix errors: defaultrouter

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,10 +1,15 @@
-from rest_framework_nested import routers
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter  
 from .views import ConversationViewSet, MessageViewSet
 from django.urls import path, include
 
-router = routers.NestedDefaultRouter()
+
+router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
+
+
+_ = NestedDefaultRouter(router, r'conversations', lookup='conversation')
 
 urlpatterns = [
     path('', include(router.urls)),
