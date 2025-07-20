d1e9eb4d00afa09619241eea7b52ca6aba4dbd89
ELIUZ <osongoeliuz14@gmail.com>
2025-07-20 18:53:03 +0300

    fix error: correct views file

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,7 +1,7 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
@@ -11,12 +11,32 @@ class ConversationViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['participants']
-    search_fields = ['participants__first_name', 'participants__last_name']
+    search_fields = ['participants__username']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        conversation = serializer.save()
+        return Response({
+            "id": conversation.id,
+            "participants": [user.username for user in conversation.participants.all()]
+        }, status=status.HTTP_201_CREATED)
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['conversation', 'sender']
+    filterset_fields = ['conversation']
     search_fields = ['message_body']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        message = serializer.save()
+        return Response({
+            "id": message.id,
+            "message_body": message.message_body,
+            "sender": message.sender.username,
+            "conversation": message.conversation.id
+        }, status=status.HTTP_201_CREATED)
