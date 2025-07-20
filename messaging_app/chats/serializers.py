a86584ab6d94e07072f28094264f632404185949
ELIUZ <osongoeliuz14@gmail.com>
2025-07-20 18:04:33 +0300

    fix error: revised ConversationSerializer

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -6,18 +6,20 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role']
 
-
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    message_body = serializers.CharField()
 
     class Meta:
         model = Message
         fields = ['message_id', 'sender', 'message_body', 'sent_at']
 
-
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
-    messages = MessageSerializer(many=True, read_only=True, source='message_set')
+    messages = MessageSerializer(many=True, read_only=True)
+
+    conversation_id = serializers.CharField()
+    created_at = serializers.DateTimeField()
 
     class Meta:
         model = Conversation
