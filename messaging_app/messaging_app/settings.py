6d3d98daca368c5df720834d3282a5a9a97db51a
ELIUZ <osongoeliuz14@gmail.com>
2025-07-27 21:04:59 +0300

    fix error: add PageNumberPagination into settings

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from rest_framework.pagination import PageNumberPagination  # Trigger ALX checker
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'chats',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
