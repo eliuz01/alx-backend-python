0334bbadb5379f68d4b0016c791f56f6bbb51740
ELIUZ <osongoeliuz14@gmail.com>
2025-08-10 17:26:45 +0300

    fix error: addited_by

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -7,6 +7,13 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    edited_by = models.ForeignKey(  
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='edited_messages'
+    )
 
     def __str__(self):
         return f"From {self.sender.username} to {self.receiver.username}"
